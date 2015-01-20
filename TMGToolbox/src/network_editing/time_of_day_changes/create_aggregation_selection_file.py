59c5154258b049a6842f38eacebad4c032f39e57
mattaustin222 <matt.austin@mail.utoronto.ca>
2015-01-20 16:47:41 -0500

    Agg selection file creation tool finished

--- TMGToolbox/src/network_editing/time_of_day_changes/create_aggregation_selection_file.py
+++ TMGToolbox/src/network_editing/time_of_day_changes/create_aggregation_selection_file.py
@@ -21,12 +21,13 @@
 '''
 Create Aggregation Selection File
 
-    Authors: 
+    Authors: mattaustin222
 
-    Latest revision by: 
+    Latest revision by: mattaustin222
     
     
-    [Description]
+    Creates the .csv file required for using the Create Transit Time Period tool.
+    Currently, line groups are defined within this tool code.
         
 '''
 #---VERSION HISTORY
@@ -39,6 +40,8 @@ import inro.modeller as _m
 import traceback as _traceback
 from contextlib import contextmanager
 from contextlib import nested
+import csv
+from inro.emme.core.exception import ModuleError
 _MODELLER = _m.Modeller() #Instantiate Modeller once.
 _util = _MODELLER.module('tmg.common.utilities')
 _tmgTPB = _MODELLER.module('tmg.common.TMG_tool_page_builder')
@@ -61,20 +64,20 @@ class CreateAggregationSelectionFile(_m.Tool()):
     
     BaseScenario = _m.Attribute(_m.InstanceType)
     
-    AggTypeSelectionFile = _m.Attribute(str)
+    ExportFile = _m.Attribute(str)
 
-    TorontoSurfaceAgg = _m.Attribute(str)
-    TorontoSubwayAgg = _m.Attribute(str)
-    MississaugaAgg = _m.Attribute(str)
-    DurhamAgg = _m.Attribute(str)
-    YorkAgg = _m.Attribute(str)
-    BramptonAgg = _m.Attribute(str)
-    OakvilleAgg = _m.Attribute(str)
-    BurlingtonAgg = _m.Attribute(str)
-    MiltonAgg = _m.Attribute(str)
-    HSRAgg = _m.Attribute(str)
-    GORailAgg = _m.Attribute(str)
-    GOBusAgg = _m.Attribute(str)
+    TorontoSurfaceAgg = _m.Attribute(int)
+    TorontoSubwayAgg = _m.Attribute(int)
+    MississaugaAgg = _m.Attribute(int)
+    DurhamAgg = _m.Attribute(int)
+    YorkAgg = _m.Attribute(int)
+    BramptonAgg = _m.Attribute(int)
+    OakvilleAgg = _m.Attribute(int)
+    BurlingtonAgg = _m.Attribute(int)
+    MiltonAgg = _m.Attribute(int)
+    HSRAgg = _m.Attribute(int)
+    GORailAgg = _m.Attribute(int)
+    GOBusAgg = _m.Attribute(int)
         
     def __init__(self):
         #---Init internal variables
@@ -82,13 +85,26 @@ class CreateAggregationSelectionFile(_m.Tool()):
         
         #---Set the defaults of parameters used by Modeller
         self.BaseScenario = _MODELLER.scenario #Default is primary scenario
+        self.TorontoSurfaceAgg = 1
+        self.TorontoSubwayAgg = 1
+        self.MississaugaAgg = 1
+        self.DurhamAgg = 1
+        self.YorkAgg = 1
+        self.BramptonAgg = 1
+        self.OakvilleAgg = 1
+        self.BurlingtonAgg = 1
+        self.MiltonAgg = 1
+        self.HSRAgg = 1
+        self.GORailAgg = 2
+        self.GOBusAgg = 2
     
     def page(self):
         pb = _tmgTPB.TmgToolPageBuilder(self, title="Create Aggregation Selection File v%s" %self.version,
                      description="Creates an aggregation selection file that can be \
                          used with the Create Transit Time Period Tool. Every line in \
                          the network requires a aggregation type and this tool automates \
-                         that process.",
+                         that process. For each line group listed, please select an \
+                         aggregation type.",
                      branding_text="- TMG Toolbox")
         
         if self.tool_run_msg != "": # to display messages in the page
@@ -107,13 +123,43 @@ class CreateAggregationSelectionFile(_m.Tool()):
         
         pb.add_header("TOOL INPUTS")
         
-        #Add selections here for :
-        #TTC Bus
-        #TTC Streetcar
-        #TTC Subway
-        #Mississauga
-        #YRT/VIVA
-        #etc.
+        keyval1 = {1:'Naive', 2:'Average'}
+        pb.add_radio_group(tool_attribute_name='TorontoSurfaceAgg', 
+                           keyvalues= keyval1,
+                           title= "TTC Surface Routes")
+        pb.add_radio_group(tool_attribute_name='TorontoSubwayAgg', 
+                           keyvalues= keyval1,
+                           title= "TTC Subway Routes")
+        pb.add_radio_group(tool_attribute_name='MississaugaAgg', 
+                           keyvalues= keyval1,
+                           title= "MiWay Routes")
+        pb.add_radio_group(tool_attribute_name='DurhamAgg', 
+                           keyvalues= keyval1,
+                           title= "Durham Routes")
+        pb.add_radio_group(tool_attribute_name='YorkAgg', 
+                           keyvalues= keyval1,
+                           title= "YRT/VIVA Routes")
+        pb.add_radio_group(tool_attribute_name='BramptonAgg', 
+                           keyvalues= keyval1,
+                           title= "Brampton Routes")
+        pb.add_radio_group(tool_attribute_name='OakvilleAgg', 
+                           keyvalues= keyval1,
+                           title= "Oakville Routes")
+        pb.add_radio_group(tool_attribute_name='BurlingtonAgg', 
+                           keyvalues= keyval1,
+                           title= "Burlington Routes")
+        pb.add_radio_group(tool_attribute_name='MiltonAgg', 
+                           keyvalues= keyval1,
+                           title= "Milton Routes")
+        pb.add_radio_group(tool_attribute_name='HSRAgg', 
+                           keyvalues= keyval1,
+                           title= "HSR Routes")
+        pb.add_radio_group(tool_attribute_name='GORailAgg', 
+                           keyvalues= keyval1,
+                           title= "GO Rail Routes")
+        pb.add_radio_group(tool_attribute_name='GOBusAgg', 
+                           keyvalues= keyval1,
+                           title= "GO Bus Routes")
 
         return pb.render()
     
@@ -139,11 +185,16 @@ class CreateAggregationSelectionFile(_m.Tool()):
         with _m.logbook_trace(name="{classname} v{version}".format(classname=(self.__class__.__name__), version=self.version),
                                      attributes=self._GetAtts()):
             
-            network = self.BaseScenario.get_network()
-            self.TRACKER.completeTask()
-            print "Loaded network"
+            groups = self._DefineGroups()
             
-            #
+            with _util.tempExtraAttributeMANAGER(self.BaseScenario, 'TRANSIT_LINE', default = 1, description = "Agg Type") as aggType:
+                self._AssignAggType(groups, aggType.id)
+                network = self.BaseScenario.get_network()
+                self.TRACKER.completeTask()
+                print "Loaded network" 
+                self._WriteAggSelections(network, aggType.id)
+
+            
 
     ##########################################################################################################    
     
@@ -156,3 +207,68 @@ class CreateAggregationSelectionFile(_m.Tool()):
                 "self": self.__MODELLER_NAMESPACE__}
             
         return atts 
+
+    def _DefineGroups(self):
+        GroupIds = []
+        GroupIds.append(['T_____', self.TorontoSurfaceAgg, 'TTC Surface'])
+        GroupIds.append(['TS____', self.TorontoSubwayAgg, 'TTC Subway']) #must follow surface routes, since pattern is a subset
+        GroupIds.append(['M_____', self.MississaugaAgg, 'MiWay'])
+        GroupIds.append(['D_____', self.DurhamAgg, 'Durham'])
+        GroupIds.append(['Y_____', self.YorkAgg, 'YRT/VIVA'])
+        GroupIds.append(['B_____', self.BramptonAgg, 'Brampton'])
+        GroupIds.append(['HO____', self.OakvilleAgg, 'Oakville'])
+        GroupIds.append(['HB____', self.BurlingtonAgg, 'Burlington'])
+        GroupIds.append(['HM____', self.MiltonAgg, 'Milton'])
+        GroupIds.append(['W_____', self.HSRAgg, 'HSR'])
+        GroupIds.append(['GT____', self.GORailAgg, 'GO Rail'])
+        GroupIds.append(['GB____', self.GOBusAgg, 'GO Bus'])
+        return GroupIds
+
+    def _AssignAggType(self, groups, aggTypeId):
+
+        tool = _MODELLER.tool('inro.emme.network_calculation.network_calculator')
+        def getSpec(value, selection):
+            return {
+                "result": aggTypeId,
+                "expression": str(value),
+                "aggregation": None,
+                "selections": {
+                    "transit_line": selection
+                },
+                "type": "NETWORK_CALCULATION"
+            }
+
+        i = 0
+        for items in groups:
+            selector = groups[i][0]
+            groupAgg = groups[i][1]
+            id = groups[i][2]
+            i += 1
+            spec = getSpec(groupAgg, selector)
+            try:
+                tool(spec, scenario= self.BaseScenario)
+            except ModuleError:
+                msg = "Emme runtime error processing line group '%s'." %id
+                _m.logbook_write(msg)
+                print msg
+                raise
+
+            msg = "Loaded group %s: %s" %(groupAgg, id)
+            print msg
+            _m.logbook_write(msg)
+
+    def _WriteAggSelections(self, network, aggTypeId):
+        with open(self.ExportFile, 'wb') as csvfile:
+            aggWrite = csv.writer(csvfile, delimiter = ',')
+            aggWrite.writerow(['emme_id', 'agg_type'])
+            for line in network.transit_lines():
+                if line[aggTypeId] == 1: aggWrite.writerow([line.id, 'naive'])                    
+                else : aggWrite.writerow([line.id, 'average'])                    
+                
+    @_m.method(return_type=_m.TupleType)
+    def percent_completed(self):
+        return self.TRACKER.getProgress()
+                
+    @_m.method(return_type=unicode)
+    def tool_run_msg_status(self):
+        return self.tool_run_msg
