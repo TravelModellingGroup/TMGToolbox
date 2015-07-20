b2e8668226dca3467f6d055b3379996b32407137
mattaustin222 <matt.austin@mail.utoronto.ca>
2015-07-20 16:14:02 -0400

    Updated to allow for multiple worksheets to be inputted. Full paths or comma-separated Emme link hierarchies must be separated by a pipe "|"

--- TMGToolbox/src/XTMF_internal/export_worksheet_table.py
+++ TMGToolbox/src/XTMF_internal/export_worksheet_table.py
@@ -57,9 +57,9 @@ class ExportWorksheetTable(_m.Tool()):
 
      #---PARAMETERS
     xtmf_ScenarioNumber = _m.Attribute(str)
-    xtmf_WorksheetPath = _m.Attribute(str)
+    xtmf_WorksheetPaths = _m.Attribute(str)
     FilePath = _m.Attribute(str) 
-    WorksheetPath = _m.Attribute(list)
+    WorksheetPaths = _m.Attribute(list)
 
     Scenario = _m.Attribute(_m.InstanceType)
 
@@ -70,72 +70,76 @@ class ExportWorksheetTable(_m.Tool()):
         
         #---Set the defaults of parameters used by Modeller
         self.Scenario = _MODELLER.scenario #Default is primary scenario   
+        self.WorksheetPaths = []
 
     def run(self):
         self.tool_run_msg = ""
-        #testPath = "C:\Users\matta_000\Documents\EMME Projects\SmartTrack\Worksheets\TestModes.emt"
+        #testPath = "C:\Users\matta_000\Documents\EMME Projects\SmartTrack\Worksheets\TestModes.emt|C:\Users\matta_000\Documents\EMME Projects\SmartTrack\Worksheets\TestModes2.emt"
         #self.FilePath = "C:\Users\matta_000\Documents\XTMF\Projects\Test"
 
-        #if os.path.isfile(testPath): # optionally provide full path to worksheet file
-        #    # check to see if the file is already in the correct location
-        #    testPath = os.path.abspath(testPath)
-        #    head, tail = os.path.split(testPath)            
-        #    worksheetLocation = os.path.abspath(os.path.join(os.path.dirname(emmebankLocation), os.pardir, 'Worksheets'))
-        #with open(testPath, 'r') as f:
-        #    checkFile = False
-        #    for line in f:
-        #        if "name = " in line.lower(): # this is the convention for the table title in the table file
-        #            tableTitle = line[7:].strip()      
-        #            checkFile = True
-        #            break
-        #    if not checkFile:
-        #        raise Exception("File is not a valid Emme table file")
-        #if head == worksheetLocation:                
-        #    self.WorksheetPath = [[tableTitle]] # set the path as a list of lists to feed in to the INRO tool
-        #    print tableTitle
-        #else:
-        #    try:
-        #        shutil.copy2(testPath, worksheetLocation) # copy the worksheet file to the project worksheet directory
-        #        self.WorksheetPath = [[tableTitle]]
-        #    except:
-        #        raise Exception("Failed to copy worksheet to project/Worksheets directory")
+        #for paths in testPath.split("|"):
+        #    if os.path.isfile(paths): # optionally provide full path to worksheet file
+        #        # check to see if the file is already in the correct location
+        #        head, tail = os.path.split(paths)
+        #        worksheetLocation = os.path.abspath(os.path.join(os.path.dirname(emmebankLocation), os.pardir, 'Worksheets'))
+        #        with open(paths, 'r') as f:
+        #            checkFile = False
+        #            for line in f:
+        #                if "name = " in line.lower(): # this is the convention for the table title in the table file
+        #                    tableTitle = line[7:].strip() # grab the table name and remove formatting      
+        #                    checkFile = True
+        #                    break
+        #            if not checkFile:
+        #                raise Exception("File is not a valid Emme table file")
+        #        if head == worksheetLocation:                
+        #            self.WorksheetPaths.append([tableTitle]) # set the table title as a list of lists to feed in to the INRO tool
+        #        else:
+        #            try:
+        #                shutil.copy2(paths, worksheetLocation) # copy the worksheet file to the project worksheet directory
+        #                self.WorksheetPaths.append([tableTitle])
+        #            except:
+        #                raise Exception("Failed to copy worksheet to project/Worksheets directory")
+        #    else:        
+        #        self.WorksheetPaths.append(paths.split(",")) # currently can only handle one path
 
         #try:
         #    self._Execute()
         #except Exception, e:
         #    msg = str(e) + "\n" + _traceback.format_exc(e)
         #    raise Exception(msg)
+
         
-    def __call__(self, xtmf_ScenarioNumber, xtmf_WorksheetPath, FilePath):
+    def __call__(self, xtmf_ScenarioNumber, xtmf_WorksheetPaths, FilePath):
         self.tool_run_msg = ""
 
         self.Scenario = _MODELLER.emmebank.scenario(xtmf_ScenarioNumber)
         if (self.Scenario == None):
             raise Exception("Scenario %s was not found!" %xtmf_ScenarioNumber)
-
-        if os.path.isfile(xtmf_WorksheetPath): # optionally provide full path to worksheet file
-            # check to see if the file is already in the correct location
-            head, tail = os.path.split(xtmf_WorksheetPath)
-            worksheetLocation = os.path.abspath(os.path.join(os.path.dirname(emmebankLocation), os.pardir, 'Worksheets'))
-            with open(xtmf_WorksheetPath, 'r') as f:
-                checkFile = False
-                for line in f:
-                    if "name = " in line.lower(): # this is the convention for the table title in the table file
-                        tableTitle = line[7:].strip() # grab the table name and remove formatting      
-                        checkFile = True
-                        break
-                if not checkFile:
-                    raise Exception("File is not a valid Emme table file")
-            if head == worksheetLocation:                
-                self.WorksheetPath = [[tableTitle]] # set the table title as a list of lists to feed in to the INRO tool
-            else:
-                try:
-                    shutil.copy2(xtmf_WorksheetPath, worksheetLocation) # copy the worksheet file to the project worksheet directory
-                    self.WorksheetPath = [[tableTitle]]
-                except:
-                    raise Exception("Failed to copy worksheet to project/Worksheets directory")
-        else:        
-            self.WorksheetPath = [xtmf_WorksheetPath.split(",")] # currently can only handle one path
+                
+        for paths in xtmf_WorksheetPaths.split("|"):
+            if os.path.isfile(paths): # optionally provide full path to worksheet file
+                # check to see if the file is already in the correct location
+                head, tail = os.path.split(paths)
+                worksheetLocation = os.path.abspath(os.path.join(os.path.dirname(emmebankLocation), os.pardir, 'Worksheets'))
+                with open(paths, 'r') as f:
+                    checkFile = False
+                    for line in f:
+                        if "name = " in line.lower(): # this is the convention for the table title in the table file
+                            tableTitle = line[7:].strip() # grab the table name and remove formatting      
+                            checkFile = True
+                            break
+                    if not checkFile:
+                        raise Exception("File is not a valid Emme table file")
+                if head == worksheetLocation:                
+                    self.WorksheetPaths.append([tableTitle]) # set the table title as a list of lists to feed in to the INRO tool
+                else:
+                    try:
+                        shutil.copy2(paths, worksheetLocation) # copy the worksheet file to the project worksheet directory
+                        self.WorksheetPaths.append([tableTitle])
+                    except:
+                        raise Exception("Failed to copy worksheet to project/Worksheets directory")
+            else:        
+                self.WorksheetPaths.append(paths.split(",")) # currently can only handle one path
 
         try:
             self._Execute()
@@ -152,7 +156,7 @@ class ExportWorksheetTable(_m.Tool()):
             
             logbookLocation = os.path.abspath(os.path.join(os.path.dirname(emmebankLocation), os.pardir, 'Logbook'))
             initialDir = os.listdir(logbookLocation)
-            LogTable(worksheet_items_or_folders=self.WorksheetPath, field_separator=",")
+            LogTable(worksheet_items_or_folders=self.WorksheetPaths, field_separator=",")
             finalDir = os.listdir(logbookLocation)
             newItems = []
             for item in finalDir:
