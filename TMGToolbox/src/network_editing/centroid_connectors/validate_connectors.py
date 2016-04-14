25b69a4c8fcdc9923389d5606e01a0463a28a577
Monika N <m.nasterska@mail.utoronto.ca>
2016-04-14 15:33:11 -0400

    Added zone and link selectors

--- TMGToolbox/src/network_editing/centroid_connectors/validate_connectors.py
+++ TMGToolbox/src/network_editing/centroid_connectors/validate_connectors.py
@@ -33,6 +33,7 @@
 
 import inro.modeller as _m
 import traceback as _traceback
+from contextlib import nested
 _MODELLER = _m.Modeller()
 _util = _MODELLER.module('tmg.common.utilities')
 _tmgTPB = _MODELLER.module('tmg.common.TMG_tool_page_builder')
@@ -46,11 +47,15 @@ class ValidateConnectors(_m.Tool()):
     FlagAttribute = _m.Attribute(_m.InstanceType)
     criteria = _m.Attribute(int)
     intCutoff = _m.Attribute(int)
+    InfeasibleLinkSelector = _m.Attribute(str)
+    ZoneSelector = _m.Attribute(str)
 
 
     def __init__(self):
         self.Scenario = _MODELLER.scenario
         self._tracker = _util.ProgressTracker(5)
+        self.ZoneSelector = "all"
+        self.InfeasibleLinkSelector = "mode = t"
 
     def page(self):
         pb = _tmgTPB.TmgToolPageBuilder(self, title="Validate Centroid Connectors v%s" %self.version,
@@ -76,6 +81,12 @@ class ValidateConnectors(_m.Tool()):
                         title="Select Scenario",
                         allow_none=False)
 
+        pb.add_text_box(tool_attribute_name='ZoneSelector',
+                size=300,
+                multi_line=True,
+                title='Specify which zones to validate.',
+                note="Formatted the same as for a network calculation.")
+
         keyvalues = {1: 'number of connectors is less than the cutoff value', 
                      2: 'number of connectors at intersections is greater than the cutoff value',
                      3: 'number of connectors to other connectors is greater than the cutoff value',
@@ -89,12 +100,19 @@ class ValidateConnectors(_m.Tool()):
                         size=2,
                         title = "cutoff value")
 
+        pb.add_text_box(tool_attribute_name='InfeasibleLinkSelector',
+                        size=300,
+                        multi_line=True,
+                        title='Links which do not count as intersections or other connectors.',
+                        note="Formatted the same as for a network calculation.")
 
         pb.add_select_attribute(tool_attribute_name="FlagAttribute",
             filter='NODE',
             allow_none=True,
             title="Attribute to flag zones.",
             note="Zones which meet the selected criteria will be flagged as True/1.")
+
+
 
         return pb.render()
 
@@ -114,72 +132,131 @@ class ValidateConnectors(_m.Tool()):
                 e, _traceback.format_exc(e))
             raise
 
+    #########################################################################################
+
     def _execute(self):
-        network = self.Scenario.get_network()
 
-        flagged_count = 0
+        with _m.logbook_trace(name="{0} v{1}".format(self.__class__.__name__,self.version)):
+            flagged_count = 0
 
-        if not self.criteria or not self.intCutoff or not self.FlagAttribute:
-            raise Exception ("Criteria, cutoff value and/or flag attribute not specified")
+            if not self.criteria or not self.intCutoff or not self.FlagAttribute:
+                raise Exception ("Criteria, cutoff value and/or flag attribute not specified")
 
-        for node in network.centroids():
+            with nested(_util.tempExtraAttributeMANAGER(self.Scenario, 'NODE'), _util.tempExtraAttributeMANAGER(self.Scenario, 'LINK'))\
+             as (selectZone, excludeLink):
+
+                    self._applyInfeasibleLinkFilter(excludeLink.id)
+                    self._applyZoneFilter(selectZone.id) 
+                    network = self.Scenario.get_network()
+
+                    for node in network.centroids():
+                    
+                        #initialize variables
+                        node[self.FlagAttribute.id] = False
+
+                        if node[selectZone.id] == 1:
+
+                            intersections = 0
+                            connectors = 0
+                            conn_centroids = 0
+                            min_distance = float("inf")
+                    
     
-            #initialize variables
-            node["@isbad"] = False
-            intersections = 0
-            connectors = 0
-            conn_centroids = 0
-            min_distance = float("inf")
+                            for connector in node.outgoing_links():
+        
+                                #count the number of connectors
+                                connectors += 1
+        
+                                #find the minimum connector length
+                                if connector.length < min_distance:
+                                    min_distance = connector.length
+        
+                                #determine if connector is connecting to an intersection
+                                #or another connector
+                                connected_to_centroid = False
+                                degree = 0    
+                                for second_link in connector.j_node.outgoing_links():
+                                    if second_link[excludeLink.id] == 0:
+                                        second_node = second_link.j_node
+                                        if second_node <> node:
+                                            if second_node.is_centroid == False:
+                                                degree += 1
+                                            else:
+                                                connected_to_centroid = True
+                                if degree > 2:
+                                    intersections +=1
+                                if connected_to_centroid:
+                                    conn_centroids += 1
     
-            for connector in node.outgoing_links():
-        
-                #count the number of connectors
-                connectors += 1
-        
-                #find the minimum connector length
-                if connector.length < min_distance:
-                    min_distance = connector.length
-        
-                #determine if connector is connecting to an intersection
-                #or another connector
-                connected_to_centroid = False
-                degree = 0    
-                for second_link in connector.j_node.outgoing_links():
-                    second_node = second_link.j_node
-                    if second_node <> node:
-                        if second_node.is_centroid == False:
-                            degree += 1
-                        else:
-                            connected_to_centroid = True
-                if degree > 2:
-                    intersections +=1
-                if connected_to_centroid:
-                    conn_centroids += 1
-    
-            #criteria = number of connectors
-            if self.criteria == 1:
-                if connectors < self.intCutoff:
-                    node[self.FlagAttribute.id] = True
-                    flagged_count += 1
-            #criteria = number of connectors at intersections
-            elif self.criteria == 2:
-                if intersections > self.intCutoff:
-                    node[self.FlagAttribute.id] = True
-                    flagged_count += 1
-            #criteria = number of connectors to other connectors
-            elif self.criteria == 3:
-                if conn_centroids > self.intCutoff:
-                    node[self.FlagAttribute.id] = True
-                    flagged_count += 1
-            #criteria = minimum distance
-            elif self.criteria == 4:
-                if min_distance > self.intCutoff:
-                    node[self.FlagAttribute.id] = True
-                    flagged_count += 1
+                            #criteria = number of connectors
+                            if self.criteria == 1:
+                                if connectors < self.intCutoff:
+                                    node[self.FlagAttribute.id] = True
+                                    flagged_count += 1
+                            #criteria = number of connectors at intersections
+                            elif self.criteria == 2:
+                                if intersections > self.intCutoff:
+                                    node[self.FlagAttribute.id] = True
+                                    flagged_count += 1
+                            #criteria = number of connectors to other connectors
+                            elif self.criteria == 3:
+                                if conn_centroids > self.intCutoff:
+                                    node[self.FlagAttribute.id] = True
+                                    flagged_count += 1
+                            #criteria = minimum distance
+                            elif self.criteria == 4:
+                                if min_distance > self.intCutoff:
+                                    node[self.FlagAttribute.id] = True
+                                    flagged_count += 1
 
-               
-
-        self.Scenario.publish_network(network, resolve_attributes= True)
-        print "published network"
+            self.Scenario.publish_network(network, resolve_attributes= True)
 
         self.tool_run_msg = _m.PageBuilder.format_info(" {0} zones were flagged.".format(flagged_count))
+
+#----Filters and Exclusions----------------------------------------------------------------------------
+
+    def _applyInfeasibleLinkFilter(self, attributeId): 
+        if self.InfeasibleLinkSelector == "" or self.InfeasibleLinkSelector == None:
+            self._tracker.completeTask() 
+            return
+        else:
+            spec = {
+                    "result": attributeId,
+                    "expression": "1",
+                    "aggregation": None,
+                    "selections": {
+                                   "link": self.InfeasibleLinkSelector
+                                   },
+                    "type": "NETWORK_CALCULATION"
+                    }
+        tool = None
+        try:
+            tool = _m.Modeller().tool("inro.emme.network_calculation.network_calculator")
+        except Exception, e:
+            tool = _m.Modeller().tool("inro.emme.standard.network_calculation.network_calculator")
+        
+        self._tracker.runTool(tool, spec, scenario=self.Scenario)
+
+#------------------------------------------------------------------------------------------------
+
+    def _applyZoneFilter(self, attributeId):
+        if self.ZoneSelector == "" or self.ZoneSelector == None:
+            self._tracker.completeTask() 
+            return
+        else:
+            spec = {
+                    "result": attributeId,
+                    "expression": "1",
+                    "aggregation": None,
+                    "selections": {
+                                   "node": self.ZoneSelector + " and ci = 1"
+                                   },
+                    "type": "NETWORK_CALCULATION"
+                    }
+        tool = None
+        try:
+            tool = _m.Modeller().tool("inro.emme.network_calculation.network_calculator")
+        except Exception, e:
+            tool = _m.Modeller().tool("inro.emme.standard.network_calculation.network_calculator")
+        
+        self._tracker.runTool(tool, spec, scenario=self.Scenario)
