8d8bd31fb0e367daf85571d61106b7469613059f
Cheung, Brian <Brian.Cheung@wsp.com>
2024-06-04 14:44:39 -0400

    Updated Build Toolbox batchfiles for EMME 2024

--- TMGToolbox/Build Consolidated Toolbox.bat
+++ TMGToolbox/Build Consolidated Toolbox.bat
@@ -1,6 +1,28 @@
 @echo off
-if exist "%EMMEPATH%/Python27/python.exe" (
-    "%EMMEPATH%/Python27/python.exe" "%~dp0\build_toolbox.py" -c
+setlocal
+
+REM Check if the Python executables exist
+if exist "%EMMEPATH%\Python27\python.exe" (
+    set "EMME_PYTHON=%EMMEPATH%\Python27\python.exe"
+    goto BuildToolbox
+) else if exist "%EMMEPATH%\Python37\python.exe" (
+    set "EMME_PYTHON=%EMMEPATH%\Python37\python.exe"
+    goto BuildToolbox
+) else if exist "%EMMEPATH%\Python311\python.exe" (
+    set "EMME_PYTHON=%EMMEPATH%\Python311\python.exe"
+    goto BuildToolbox
 ) else (
-    "%EMMEPATH%/Python37/python.exe" "%~dp0\build_toolbox.py" -c
+    goto Error
 )
+
+:BuildToolbox
+REM Execute build_toolbox.py using the selected Python environment
+echo Found EMME Python environment at %EMME_PYTHON% & echo.
+"%EMME_PYTHON%" "%~dp0\build_toolbox.py" -c
+goto End
+
+:Error
+echo Cannot find the EMME Python environment
+goto End
+
+:End
